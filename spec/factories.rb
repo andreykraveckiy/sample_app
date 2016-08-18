b2adb6e21c3d359a16113a09636e69224c086556
Andrey Kraveckiy <andreykraveckiy@gmail.com>
2016-08-18 22:44:19 +0300

    Add pagenation. Add new fake-users. Make refactoring.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 	factory :user do
-		name	"Andrey Kravetskiy"
-		email "andkrav@example.com"
+		sequence(:name)	 { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" }
 		password "foobar"
 		password_confirmation "foobar"
 	end
